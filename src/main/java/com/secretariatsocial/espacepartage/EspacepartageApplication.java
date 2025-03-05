a4dd39ad2e546165c83a4b71c1cee7fe44e6fd46
Mehdi <chihi.med@gmail.com>
2025-03-05 22:48:15 +0100

    setup: init spring boot project

---
+++ src/main/java/com/secretariatsocial/espacepartage/EspacepartageApplication.java
@@ -0,0 +1,13 @@
+package com.secretariatsocial.espacepartage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EspacepartageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EspacepartageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/secretariatsocial/espacepartage/EspacepartageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.secretariatsocial.espacepartage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EspacepartageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
